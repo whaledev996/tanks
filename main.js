b180cc9c31814ffcfd0813f19041387fb20efb2d
whaledev <whaledev996@gmail.com>
2023-08-04 00:04:26 -0400

    basic movement working

--- main.js
+++ main.js
@@ -6,3 +6,47 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 2, 2, 0.5);
+const material = new THREE.MeshBasicMaterial( { color: 0x4287f5 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 25;
+
+const keys = new Set();
+const arrowKeys = new Set(["ArrowLeft", "ArrowUp", "ArrowRight", "ArrowDown"]);
+window.addEventListener("keyup", (e) => {
+    const key = e.key;
+    if (arrowKeys.has(key)) {
+        keys.delete(key);
+    }
+
+});
+window.addEventListener("keydown", (e) => {
+    const key = e.key;
+    if(arrowKeys.has(key)) {
+        keys.add(key);
+    }
+});
+
+function animate() {
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+    if (keys.has("ArrowLeft")) {
+        cube.translateX(-0.1);
+    }
+
+    if (keys.has("ArrowUp")) {
+        cube.translateY(0.1);
+    }
+
+    if (keys.has("ArrowRight")) {
+        cube.translateX(0.1);
+    }
+
+    if (keys.has("ArrowDown")) {
+        cube.translateY(-0.1);
+    }
+}
+animate();
