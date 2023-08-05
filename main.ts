aad5390ed5a652c17ffb6c85fe1cec5fc2c9b8a7
whaledev <whaledev996@gmail.com>
2023-08-05 01:18:25 -0400

    basic box obstacle

--- main.ts
+++ main.ts
@@ -13,6 +13,11 @@ const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 25;
+camera.position.y = -6;
+//camera.position.x = 12;
+//camera.rotation.x += 1;
+camera.lookAt(0, 0, 0);
+//console.log(camera.lo)
 
 const keys = new Set();
 const arrowKeys = new Set(["ArrowLeft", "ArrowUp", "ArrowRight", "ArrowDown"]);
@@ -30,9 +35,26 @@ window.addEventListener("keydown", (e) => {
     }
 });
 
+const boxGeometry = new THREE.BoxGeometry(5, 2, 5);
+const loader = new THREE.TextureLoader();
+const boxMesh = new THREE.MeshStandardMaterial({
+    map: loader.load('https://threejsfundamentals.org/threejs/lessons/resources/images/compressed-but-large-wood-texture.jpg')
+ });
+boxMesh.color.convertSRGBToLinear();
+const standardBox = new THREE.Mesh(boxGeometry, boxMesh);
+standardBox.position.set(0, 10, 5);
+scene.add(standardBox);
+
+const light = new THREE.HemisphereLight(0xddeeff, 0x202020, 3);
+//light.position.set(0, 0, 0);
+scene.add(light);
+
+
 function animate() {
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
+    //boxMesh.rotateZ(0.01);
+    //standardBox.rotateX(0.01);
     if (keys.has("ArrowLeft")) {
         cube.rotateZ(0.02);
     }
