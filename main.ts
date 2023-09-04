c59689bb17f4e450beb89c1e33979e4d885b983d
whaledev <whaledev996@gmail.com>
2023-09-03 23:06:07 -0700

    bounding box collisions

--- main.ts
+++ main.ts
@@ -11,6 +11,9 @@ const geometry = new THREE.BoxGeometry( 2, 2, 0.5);
 const material = new THREE.MeshBasicMaterial( { color: 0x4287f5 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
+// const test = cube.geometry; 
+// console.log(test);
+// const boundingBox = cube.geometry.computeBoundingBox();
 
 camera.position.z = 25;
 camera.position.y = -6;
@@ -20,9 +23,10 @@ camera.lookAt(0, 0, 0);
 //console.log(camera.lo)
 
 const keys = new Set();
-const arrowKeys = new Set(["ArrowLeft", "ArrowUp", "ArrowRight", "ArrowDown"]);
+const arrowKeys = new Set(["w", "a", "s", "d"]);
 window.addEventListener("keyup", (e) => {
     const key = e.key;
+    console.log(key);
     if (arrowKeys.has(key)) {
         keys.delete(key);
     }
@@ -35,40 +39,71 @@ window.addEventListener("keydown", (e) => {
     }
 });
 
-const boxGeometry = new THREE.BoxGeometry(5, 2, 5);
+const boxGeometry = new THREE.BoxGeometry(20, 2, 5);
 const loader = new THREE.TextureLoader();
 const boxMesh = new THREE.MeshStandardMaterial({
     map: loader.load('https://threejsfundamentals.org/threejs/lessons/resources/images/compressed-but-large-wood-texture.jpg')
  });
 boxMesh.color.convertSRGBToLinear();
 const standardBox = new THREE.Mesh(boxGeometry, boxMesh);
-standardBox.position.set(0, 10, 5);
+standardBox.position.set(0, 11, 2);
 scene.add(standardBox);
 
-const light = new THREE.HemisphereLight(0xddeeff, 0x202020, 3);
-//light.position.set(0, 0, 0);
+const light = new THREE.HemisphereLight(0xddeeff, 0x202020, 5);
+light.position.set(0, 0, 0);
 scene.add(light);
+
+const helper = new THREE.BoxHelper(cube);
+const boundingBox = new THREE.Box3().setFromObject(cube);
+const boundingBox2 = new THREE.Box3().setFromObject(standardBox);
+console.log(boundingBox2.min)
+console.log(boundingBox2.max)
+//cube.geometry.compu
 
 
 function animate() {
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
+    boundingBox.setFromObject(cube);
+    // helper.update();
     //boxMesh.rotateZ(0.01);
     //standardBox.rotateX(0.01);
-    if (keys.has("ArrowLeft")) {
+    // console.log(cube.position)
+
+	//this.position.add( _v1.multiplyScalar( distance ) );
+
+    //console.log(moving_up)
+
+    
+    //test_dir.multiplyScalar(0.05)
+
+    if (keys.has("a")) {
         cube.rotateZ(0.02);
     }
 
-    if (keys.has("ArrowUp")) {
+    if (keys.has("w")) {
+        console.log(boundingBox.min);
+        console.log(boundingBox.max);
+        console.log(boundingBox.intersectsBox(boundingBox2));
+        // if (cube.position.y < standardBox.position.y - 1) {
         cube.translateY(0.05);
+        if (boundingBox.intersectsBox(boundingBox2)) {
+            cube.translateY(-0.05);
+        }
+        // }
     }
 
-    if (keys.has("ArrowRight")) {
+    if (keys.has("d")) {
         cube.rotateZ(-0.02);
     }
 
-    if (keys.has("ArrowDown")) {
+    if (keys.has("s")) {
+        // if (cube.position.y < standardBox.position.y) {
         cube.translateY(-0.05);
+        if (boundingBox.intersectsBox(boundingBox2)) {
+            cube.translateY(0.05);
+        }
+        // }
     }
 }
 animate();
