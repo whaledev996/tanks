2f011e4099b9ecc8df02f9f53285cc1d724095be
whaledev <whaledev996@gmail.com>
2023-11-14 19:33:00 -0500

    multiple projectiles + some cleanup

--- main.ts
+++ main.ts
@@ -7,35 +7,29 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 2, 2, 0.5);
+const geometry = new THREE.BoxGeometry( 1, 1, 0.5);
 const material = new THREE.MeshBasicMaterial( { color: 0x4287f5 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
-// const test = cube.geometry; 
-// console.log(test);
-// const boundingBox = cube.geometry.computeBoundingBox();
 
 camera.position.z = 25;
 camera.position.y = -10;
-//camera.position.x = 12;
-//camera.rotation.x += 1;
 camera.lookAt(0, 0, 0);
-//console.log(camera.lo)
-//
 
 
 var vec = new THREE.Vector3(); // create once and reuse
 var pos = new THREE.Vector3(); // create once and reuse
-const projectileGeometry = new THREE.CapsuleGeometry( 0.3, 0.5, 4, 8); 
+const projectileGeometry = new THREE.CapsuleGeometry( 0.2, 0.3, 4, 8); 
 projectileGeometry.applyMatrix4( new THREE.Matrix4().makeRotationX( Math.PI / 2 ) );
+let projectiles: Array<Object> = []
 
-const projectileMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff } ); 
-const projectile = new THREE.Mesh(projectileGeometry, projectileMaterial);
-const projectileBox = new THREE.Box3().setFromObject(projectile);
-let projectileAdded = false;
-let direction = 1;
 
 window.addEventListener("mousedown", (event) => {
+    const projectileMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff } ); 
+    const projectile = new THREE.Mesh(projectileGeometry, projectileMaterial);
+    const projectileBox = new THREE.Box3().setFromObject(projectile);
+    let projectileAdded = false;
+    let direction = 1;
     // shoot projectile
     vec.set(
         ( event.clientX / window.innerWidth ) * 2 - 1,
@@ -53,35 +47,18 @@ window.addEventListener("mousedown", (event) => {
     projectile.position.x = cube.position.x;
     projectile.position.y = cube.position.y;
     projectile.position.z = cube.position.z;
-    //console.log(projectile.rotation.z);
-    //projectile.setRotationFromAxisAngle
-    //projectile.rotateZ
-    //pos.
     // find angle of rotation between tank and mouse pointer vector
     const testVector = new THREE.Vector3();
     testVector.copy(cube.position);
     const xAxis = new THREE.Vector3(0, 0, 1)
-    //const other = pos.sub(cube.position);
-    //THREE.vec
     const other = testVector.add(pos);
     const cosine = xAxis.dot(other) / (other.length());
-    // const ajngle = Math.acos(cosine);
-    //projectile.quaternion.setFromUnitVectors(xAxis, pos.clone().normalize());
-    //projectile.setRotationFromAxisAngle
-    //console.log(pos);
-    //console.log(pos.applyAxisAngle(xAxis, Math.PI/2))
     projectile.lookAt(pos);
-    //projectile.rotateZ(Math.PI/2)
 
 
     console.log(projectile.rotation);
-    //console.log(projectile.rotation.x)
-    //projectile.rotateZ(angle);
-    //projectile.rotation.z = angle;
-    //projectile.rotation.z = angle;
-    //console.log(projectile.rotation.z);
     scene.add(projectile);
-    projectileAdded = true;
+    projectiles.push([projectile, projectileBox, 1]);
 })
 
 const keys = new Set();
@@ -101,15 +78,10 @@ window.addEventListener("keydown", (e) => {
     }
 });
 
-//document.addEventListener("click", (e) => {
-    //console.log((e.clientX / window.innerWidth) * 2 - 1);
-    //console.log(-(e.clientY / window.innerHeight) * 2 + 1);
-//})
-
-const boxGeometry = new THREE.BoxGeometry(20, 2, 5);
+const boxGeometry = new THREE.BoxGeometry(20, 2, 4);
 const loader = new THREE.TextureLoader();
 const boxMesh = new THREE.MeshStandardMaterial({
-    map: loader.load('https://threejsfundamentals.org/threejs/lessons/resources/images/compressed-but-large-wood-texture.jpg')
+    map: loader.load('compressed-but-large-wood-texture.jpg')
  });
 boxMesh.color.convertSRGBToLinear();
 const standardBox = new THREE.Mesh(boxGeometry, boxMesh);
@@ -123,47 +95,22 @@ scene.add(light);
 const helper = new THREE.BoxHelper(cube);
 const boundingBox = new THREE.Box3().setFromObject(cube);
 const boundingBox2 = new THREE.Box3().setFromObject(standardBox);
-//console.log(boundingBox2.min)
-//console.log(boundingBox2.max)
-//cube.geometry.compu
-
 
 function animate() {
     requestAnimationFrame( animate );
     renderer.render( scene, camera );
     boundingBox.setFromObject(cube);
-    // helper.update();
-    //boxMesh.rotateZ(0.01);
-    //standardBox.rotateX(0.01);
-    // console.log(cube.position)
-
-	//this.position.add( _v1.multiplyScalar( distance ) );
-
-    //console.log(moving_up)
-
-    
-    //test_dir.multiplyScalar(0.05)
-    //
-    if (keys.has("y")) {
-        projectile.translateZ(0.05);
-    }
 
     if (keys.has("a")) {
         cube.rotateZ(0.02);
     }
 
     if (keys.has("w")) {
-        console.log(cube.position);
-        // console.log(boundingBox.min);
-        // console.log(boundingBox.max);
-        // console.log(boundingBox.intersectsBox(boundingBox2));
-        // if (cube.position.y < standardBox.position.y - 1) {
         cube.translateY(0.05);
         boundingBox.setFromObject(cube);
         if (boundingBox.intersectsBox(boundingBox2)) {
             cube.translateY(-0.05);
         }
-        // }
     }
 
     if (keys.has("d")) {
@@ -171,25 +118,26 @@ function animate() {
     }
 
     if (keys.has("s")) {
-        // if (cube.position.y < standardBox.position.y) {
         console.log(cube.position);
         cube.translateY(-0.05);
         boundingBox.setFromObject(cube);
         if (boundingBox.intersectsBox(boundingBox2)) {
             cube.translateY(0.05);
         }
-        // }
     }
-    if (projectileAdded) {
-        projectile.translateZ(direction * 0.05);
-        // projectile.rotateX(-0.01);
-        projectileBox.setFromObject(projectile);
-        if (projectileBox.intersectsBox(boundingBox2)) {
-            direction = -1;
-            ///rojectile.rotateX(Math.PI/2);
-            //projectile.translateZ(direction * 0.05);
-            //projectile.
-            projectile.rotation.y *= -1;
+    if (projectiles.length) {
+        for (const p of projectiles) {
+            const projectile = p[0]; 
+            const projectileBox = p[1];
+            const direction = p[2];
+            projectile.translateZ(direction * 0.03);
+            projectileBox.setFromObject(projectile);
+            if (projectileBox.intersectsBox(boundingBox2)) {
+                p[2] *= -1;
+                projectile.rotation.y *= -1;
+            }
+
+            
         }
     }
 }
